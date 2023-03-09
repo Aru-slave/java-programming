9b0a2aaab088926bdb3422b314765ed8af59fd4b
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-03-09 10:55:23 +0900

    Update ［1차］ 캐시.java

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -19,3 +19,28 @@
         }
         return answer;
     }
+import java.util.*;
+
+class Solution {
+    public static int solution(int cacheSize, String[] cities) {
+        int answer = 0;
+        if (cacheSize == 0) return cities.length * 5;
+        
+        Queue<String> cache = new LinkedList<>();
+        for (String city : cities) {
+            city = city.toLowerCase();
+            if (cache.contains(city)) {
+                cache.remove(city);  // 기존에 캐시에 있던 것을 삭제하고
+                cache.add(city);     // 맨 뒤에 다시 추가
+                answer += 1;
+            } else {
+                if (cache.size() == cacheSize) { // 캐시가 가득 찼으면
+                    cache.poll();               // 가장 오래된 것을 삭제하고
+                }
+                cache.add(city);                // 새로운 것을 추가
+                answer += 5;
+            }
+        }
+        return answer;
+    }
+}
