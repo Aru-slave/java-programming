0a662f6f7c2b2c60cd2f74f695fd435cbb3e6de4
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-03-10 10:04:05 +0900

    [level 2] Title: [3차] 압축, Time: 2.46 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -5,22 +5,23 @@ class Solution {
         List<Integer> ans = new ArrayList<>();
         Map<String,Integer> map = new HashMap<>();
         int index = 27;
-        String str = "";
+        StringBuilder sb = new StringBuilder();
         for(int i = 0; i < msg.length(); i++){
-            str = str + msg.charAt(i);
-            if(map.containsKey(str) || str.length() == 1)
+            sb.append(msg.charAt(i));
+            if(map.containsKey(sb.toString()) || sb.length() == 1)
                 continue;
             else{
-                map.put(str,index++);
-                String findStr = str.substring(0,str.length()-1);
+                map.put(sb.toString(),index++);
+                String findStr = sb.substring(0,sb.length()-1);
                 if(findStr.length() == 1) ans.add((findStr.charAt(0) - 'A') + 1);
                 else ans.add(map.get(findStr));
+                sb = new StringBuilder();
                 i--;
-                str = "";
             }
         }
-        if(str.length() == 1) ans.add((str.charAt(0) - 'A') + 1);
-        else ans.add(map.get(str));
+        if(sb.length() == 1) ans.add((sb.charAt(0) - 'A') + 1);
+        else ans.add(map.get(sb.toString()));
         return ans.stream().mapToInt(Integer::intValue).toArray();
+        
     }
 }
