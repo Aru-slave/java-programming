a2d44e3e9dd83405cd11109263b613e63fff8a3b
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-03-21 06:11:08 +0900

    [level 2] Title: 튜플, Time: 0.94 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.java
+++ 프로그래머스/lv2/64065. 튜플/튜플.java
@@ -4,26 +4,25 @@ class Solution {
     public int[] solution(String s) {
         // 문자열을 파싱하여 정수 배열로 변환
         String[] strArr = s.substring(2, s.length()-2).split("\\},\\{");
-        int[][] tuple = new int[strArr.length][];
-        for (int i = 0; i < tuple.length; i++) {
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < strArr.length; i++) {
             String[] nums = strArr[i].split(",");
-            tuple[i] = new int[nums.length];
-            for (int j = 0; j < tuple[i].length; j++) {
-                tuple[i][j] = Integer.parseInt(nums[j]);
-            }
+            List<Integer> tuple = new ArrayList<>();
+            for (int j = 0; j < nums.length; j++) {
+            tuple.add(Integer.parseInt(nums[j]));
+            list.add(tuple);
+        }
         }
         // 튜플의 원소 수를 기준으로 정렬
-        Arrays.sort(tuple, (a, b) -> Integer.compare(a.length, b.length));
+        Collections.sort(list,(a,b) -> a.toString().length() - b.toString().length());
         // 튜플의 원소들을 순서대로 추가하면서 중복된 원소는 건너뛰기
         Set<Integer> set = new HashSet<>();
-        int[] answer = new int[tuple.length];
-        int idx = 0;
-        for (int[] t : tuple) {
-            for (int num : t) {
-                if (!set.contains(num)) {
-                    set.add(num);
-                    answer[idx++] = num;
-                }
+        int[] answer = new int[list.get(list.size() - 1).size()];
+        int index = 0;
+        for(int i = 0; i < list.size(); i++){
+            for(int j = 0; j < list.get(i).size(); j++){
+                if(set.add(list.get(i).get(j)))
+                    answer[index++] = list.get(i).get(j);
             }
         }
         return answer;
