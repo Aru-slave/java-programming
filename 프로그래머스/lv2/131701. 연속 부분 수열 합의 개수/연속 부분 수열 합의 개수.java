8d877a9f8777cd621c0b33209067f8d7bd60b74b
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-03-21 10:48:00 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 217.20 ms, Memory: 133 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+public int solution(int[] elements) {
+    int n = elements.length;
+    int count = 0;
+    Set<Integer> set = new HashSet<>();
+    
+    for (int i = 0; i < n; i++) {
+        int sum = 0;
+        for (int j = i; j < i + n; j++) {
+            sum += elements[j % n];
+            if (sum > 0 && set.add(sum)) {
+                count++;
+            }
+        }
+    }
+    
+    return count;
+}
+}
