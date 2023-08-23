da572aea8b0b0ee5b0b8a05b8ed60814adb059c8
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-08-23 23:51:41 +0900

    Delete 연속 부분 수열 합의 개수.py

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++
@@ -1,9 +0,0 @@
-def solution(elements):
-    sum_set = set() 
-    n = len(elements)
-    for start in range(n):
-        sum_num = 0  
-        for end in range(start,start + n - 1):
-            sum_num += elements[end % n]
-            sum_set.add(sum_num)
-    return len(sum_set) + 1
