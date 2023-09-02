5b99da3ef7b9f076a286a5ff473906389ca60698
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-09-02 16:00:38 +0900

    [level 2] Title: 피로도, Time: 0.22 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -0,0 +1,16 @@
+def solution(k, dungeons):
+    return dfs(k, dungeons, 0, [])
+
+def dfs(k, dungeons, depth, used):
+    if depth == len(dungeons):
+        return depth  # 깊이를 반환하도록 수정
+
+    max_depth = depth  # 최대 깊이를 기록하기 위한 변수
+    for i in range(len(dungeons)):
+        if dungeons[i][0] <= k and i not in used:
+            used.append(i)
+            new_depth = dfs(k - dungeons[i][1], dungeons, depth + 1, used)
+            used.remove(i)
+            max_depth = max(max_depth, new_depth)
+
+    return max_depth
