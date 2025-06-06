fa6a3474cd1e9814ba1b1311260e37d427205708
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2025-06-06 18:42:38 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.02 ms, Memory: 97.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n, int w, int num) 
+    {
+        int cnt = 0;
+        while (num <= n) 
+        {
+	    num += (w - ((num - 1) % w) - 1) * 2 + 1;
+        cnt++;
+        }
+        return cnt;
+    }
+}
