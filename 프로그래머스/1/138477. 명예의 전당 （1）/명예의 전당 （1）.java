f6621de53da5565663faafee4d52fd47f06e12d7
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2025-06-09 23:16:35 +0900

    [level 1] Title: 명예의 전당 (1), Time: 2.18 ms, Memory: 90.8 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,18 +1,16 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
-        PriorityQueue<Integer> pq = new PriorityQueue<>(); // 오름차순: 가장 낮은 점수가 맨 위
-
-        for (int i = 0; i < score.length; i++) 
+        PriorityQueue<Integer> p = new PriorityQueue<>();
+        for(int i = 0 ; i < score.length; i++)
         {
-            pq.offer(score[i]);
-            if (pq.size() > k) 
+            p.offer(score[i]);
+            if(p.size() > k)
             {
-                pq.poll(); // k개 초과 시 가장 낮은 점수 제거
+                p.poll();
             }
-            answer[i] = pq.peek(); // 현재 상위 k개 중 가장 낮은 점수
+            answer[i] = p.peek();
         }
         return answer;
     }
